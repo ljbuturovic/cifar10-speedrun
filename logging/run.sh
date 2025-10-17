9e69ae9e07860d618eab15d2b73fe5a6de4f5911
Kerry <kerry-hiverge@github.com>
2025-10-17 03:28:06 +0000

    Added new logs on runpod a100

--- logging/run.sh
+++ logging/run.sh
@@ -1,12 +1,8 @@
-sudo nvidia-smi -pm 1
-sudo nvidia-smi -ac 1215,1410
-sudo -E nohup nice -n -20 bash -c '
+nohup bash -c '
 for i in $(seq 1 10); do
   echo "--- Starting Sequential Run $i at $(date) ---"
-  /opt/python/3.10/bin/python3 -u cifar10_speedrun.py > logs/run_${i}.log 2>&1
+  python3 -u cifar10_speedrun.py > logs/run_${i}.log 2>&1
   rm -r cifar10
 done
-sudo nvidia-smi -pm 0
-sudo nvidia-smi -rac
 echo "--- All 10 runs completed at $(date) ---"
 ' </dev/null > logs/sequential_master.log 2>&1 &
